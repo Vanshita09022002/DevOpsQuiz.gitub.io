810399b93160eda1fa0e89037691cddf0feda364
Vanshita09022002 <128284421+Vanshita09022002@users.noreply.github.com>
2023-07-18 13:46:12 +0530

    Add files via upload
    index.html file added.

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who coined the term 'DevOps'?",
+        answer: "A. Patrick Debois",
+        options: [
+            "A. Patrick Debois",
+            "B. Larry Page",
+            "C. Paul Allen",
+            "D. Warren Buffett"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following is not a DevOps tool?",
+        answer: "C. Keras",
+        options: [
+            "A. Docker",
+            "B. Kubernetes",
+            "C. Keras",
+            "D. Puppet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "When was DevOps found?",
+        answer: "B. 2009",
+        options: [
+            "A. 2006",
+            "B. 2009",
+            "C. 2007",
+            "D. 2001"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does Docker do?",
+        answer: "D. Containerization",
+        options: [
+            "A. Continuous Delivery",
+            "B. Pipeline Creation",
+            "C. Version Control",
+            "D. Containerization"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does EC2 stand for?",
+        answer: "A. Elastic Compute Cloud",
+        options: [ 
+        "A. Elastic Compute Cloud",
+        "B. Ethernet Control Cloud",
+        "C. Elastic Control Cloud",
+        "D. Ethernet Compute Center"
+        ]
+    },
+];
